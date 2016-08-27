d7a66a47d9358ff9501e2ad2cd65ced108c28bc4
kevintpeng <kpeng97@gmail.com>
2016-08-27 18:20:46 -0400

    updated entries

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo \x1b[1;32m[ 🛠 Pre-commit hook ] \x1b[0;1m Compiling static HTML\x1b[0m
-ruby generate/generate.rb && git add index.html && git commit -m "generated index.html" && open index.html
+echo -e "\e[1;32m[🛠  Pre-commit hook]\e[0;1m Compiling static HTML\e[0m"
+ruby ../../generate/generate.rb && git add ../../index.html
